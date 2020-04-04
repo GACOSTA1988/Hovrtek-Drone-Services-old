678078f23aadbe400b05c4946cb0add241591ff5
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-04-04 14:33:11 -0700

    add state and write fucntion on Client.js to handle condictional render of Projects PorjectsList Tab

--- components/client/Client.js
+++ components/client/Client.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TouchableOpacity, Text, View, StyleSheet, StatusBar, Image, ScrollView } from 'react-native';
 import ClientHeader from './header/ClientHeader';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -7,21 +7,33 @@ import ProjectList from './projectlist/ProjectList.js';
 import NewProject from './newproject/NewProject.js'
 const Drawer = createDrawerNavigator();
 
+
 const Client = () => {
+
+// Conditional Rendering state for ProjectList / New Project Tab
+  const [newProjectActive, setNewProjectAcitve ] = useState(false);
+  console.log(newProjectActive);
+
+const handleNewProjectView = () => {
+  if (newProjectActive) {
+    return <NewProject/>
+  } else {
+    return <ProjectList/>
+  }
+};
 
   return (
     <View style={styles.clientWrapper}>
 
       <StatusBar backgroundColor='blue' barStyle='light-content' />
-      <ClientHeader />
+      <ClientHeader handleNewProjectView={()=> handleNewProjectView()}/>
       <ProjectNewProjectHeader />
 
         <ScrollView style={styles.ClientListcontainer} contentContainerStyle={styles.contentClientContainer}>
           <TouchableOpacity style={styles.ClientProjectListTextWrapper}>
               <Text style={styles.clientText}>Client Projects List</Text>
           </TouchableOpacity>
-          <ProjectList />
-          <NewProject/>
+          {handleNewProjectView()}
         </ScrollView>
     </View>
   )
